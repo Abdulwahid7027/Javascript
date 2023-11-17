7e33500d3f511d1874c04f5fbd720994252ebdeb
Shaik Abdul Wahid <haseena7027@gmail.com>
2023-11-17 14:31:36 +0530

    Datatypes

---
+++ Basics/2_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1644065;
+let accountEmail = "haseena7027@gmail.com"
+var accountPassword = "1234567890"
+accountCity = "Guntur"
+let accountState = "Andhra Pradesh"
+
+/*
+   prefer not to use var
+   because it is function scoped and block scoped
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ Basics/3_datatypes.js
@@ -0,0 +1,16 @@
+/*
+ -----------------------------
+ DataTypes in JavaScript
+ -----------------------------
+ 1. Number
+ 2. String
+ 3. Boolean
+ 4. Undefined
+ 5. Null
+*/
+
+console.log(typeof 100);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null); // object
