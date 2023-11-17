cc572959b5060dc3af22385f72c6765732ad44da
Shaik Abdul Wahid <110891341+Abdulwahid7027@users.noreply.github.com>
2023-11-17 14:45:10 +0530

    Delete Basics/variable.js

--- Basics/variable.js
+++
@@ -1,7 +0,0 @@
-const accountId = 1644065;
-let accountEmail = "haseena7027@gmail.com"
-var accountPassword = "1234567890"
-accountCity = "Guntur"
-let accountState = "Andhra Pradesh"
-
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
